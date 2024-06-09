395843178904edec738873cbfb71db3ae21fd144
Sridhar <55327133+sridh-ar@users.noreply.github.com>
2024-06-09 12:42:10 +0530

    Update teamPlayer.js

--- api/teamPlayer.js
+++ api/teamPlayer.js
@@ -20,37 +20,41 @@ function handleAsync(fn) {
 // Database operations
 async function insertTeamPlayer(data) {
   return await db.oneOrNone(
-    pgpHelpers.insert(data, null, { table: "team" }) + " returning id"
+    pgpHelpers.insert(data, null, { table: "team_players" }) + " returning id"
   );
 }
 
 async function updateTeamPlayer(data) {
   return await db.oneOrNone(
-    pgpHelpers.update(data, null, { table: "team" }) + " returning id"
+    pgpHelpers.update(data, null, { table: "team_players" }) + " returning id"
   );
 }
 
 async function deleteTeamPlayerById(id) {
-  return await db.oneOrNone(`delete from team where id = '${id}'`);
+  return await db.oneOrNone(`delete from team_players where id = '${id}'`);
 }
 
 async function getAllTeamPlayer() {
-  return await db.manyOrNone(`select * from team`);
+  return await db.manyOrNone(`select * from team_players`);
+}
+
+async function getTeamPlayerForTeam(team_id) {
+  return await db.manyOrNone(`
+    select pl.* from player pl 
+    join team_players tp on tp.player_no = pl.id 
+    where tp.team_id = '${team_id}'
+  `);
 }
 
 // Routes
-teamPlayerRouter.post(
-  "/create",
-  handleAsync(async (req, res) => {
+teamPlayerRouter.post("/create", handleAsync(async (req, res) => {
     const data = req.body;
     const result = await insertTeamPlayer(data);
     res.status(200).json(result);
   })
 );
 
-teamPlayerRouter.put(
-  "/update/:id",
-  handleAsync(async (req, res) => {
+teamPlayerRouter.put("/update/:id", handleAsync(async (req, res) => {
     const data = req.body;
     let selectResult = await getAllTeamPlayer();
     Object.assign(selectResult, data);
@@ -59,18 +63,20 @@ teamPlayerRouter.put(
   })
 );
 
-teamPlayerRouter.put(
-  "/delete/:id",
-  handleAsync(async (req, res) => {
+teamPlayerRouter.put("/delete/:id", handleAsync(async (req, res) => {
     const result = await deleteTeamPlayerById(req.params.id);
     res.status(200).json(result);
   })
 );
 
-teamPlayerRouter.get(
-  "/",
-  handleAsync(async (req, res) => {
+teamPlayerRouter.get("/", handleAsync(async (req, res) => {
     const result = await getAllTeamPlayer();
+    res.status(200).json(result);
+  })
+);
+
+teamPlayerRouter.get("/team-players-list/:id", handleAsync(async (req, res) => {
+    const result = await getTeamPlayerForTeam(req.params.id);
     res.status(200).json(result);
   })
 );
